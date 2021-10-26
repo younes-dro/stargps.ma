5c55276a91f73bfc6457f86c85f18edaf6646417
younes-dro <dro66_8@hotmail.fr>
2021-10-26 16:56:30 +0100

    Remove High Tech pro Copyright

---
+++ assets/js/stargps-shortcode.js
@@ -0,0 +1,15 @@
+/* 
+ * 
+ */
+
+;
+(function($){
+    
+    $( document ).ready(function() {
+     $('div.copyright').empty().html("Copyright StarGPS®");
+});
+    
+  
+    //console.log('loaded');
+})(jQuery);
+
